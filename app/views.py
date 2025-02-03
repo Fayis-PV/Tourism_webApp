70980e3bac5c5ed66ed0f20d4f0fce9bd3eac13b
Muhammed Fayis P V <fayispvchelari@gmail.com>
2025-02-03 15:03:57 +0530

    Updates and Delete urls and views

--- app/views.py
+++ app/views.py
@@ -25,15 +25,14 @@ class DestinationListView(ListCreateAPIView):
 class DestinationDetailsView(RetrieveUpdateDestroyAPIView):
     queryset = Destination.objects.all()
     serializer_class = DestinationSerializer
+    permission_classes = [AllowAny]
 
 
 def list_destinations(request):
     response = requests.get('http://127.0.0.1:8000/list')
     if response.status_code == 200:
         datas = response.json()
-        print(datas)
     else:
-        print(response.status_code)
         datas = None
     return render(request, 'index.html', {'datas':datas})
 
@@ -44,11 +43,12 @@ def create_destination(request):
         if form.is_valid():
             form.save()
             data = form.cleaned_data
-            api_url = 'http://127.0.0.1:8000/list/'
+            api_url = 'http://127.0.0.1:8000/list'
             try:
                 response = requests.post(api_url,data=data)
-                if response.status_code == 200:
+                if response.status_code == 200 or response.status_code == 201:
                     messages.success(request, 'Successfuly created')
+                    redirect('/list_destination')
                 else:
                     messages.error(request, f'Something went wrong: {response.status_code}')
             except requests.RequestException as RE:
@@ -61,13 +61,10 @@ def create_destination(request):
 
 
 def details_destination(request,pk):
-    print('Hello')
     api_url = f'http://127.0.0.1:8000/details/{pk}'
-    print(api_url)
     data = None
     try:
         response = requests.get(api_url)
-        print(response.status_code)
         if response.status_code == 200:
             data = response.json()
             return render(request, 'details.html', {'data': data})
@@ -79,7 +76,37 @@ def details_destination(request,pk):
 
 
 def update_destination(request,pk):
-    pass
+    data = Destination.objects.get(pk = pk)
+    if request.method == 'POST':
+        form = DestinationForm(request.POST)
+        if form.is_valid():
+            api_url = f'http://127.0.0.1:8000/details/{pk}'
+            data = form.cleaned_data
+            try:
+                response = requests.put(api_url,data=data)
+                if response.status_code == 200:
+                    messages.success(request,'Successfully Updated')
+                    return redirect(f'/details_destination/{pk}')
+                else:
+                    messages.error(request, f'Something went wrong: {response.status_code}')
+            except requests.RequestException as e:
+                messages.error(request, f'Something went wrong: Error: {e}')
+        else:
+            messages.error(request, 'Please fill the form clearly')
+    else:
+        form = DestinationForm(instance=data)
+    return render(request, 'create.html', {'form':form,'pk':pk})
 
 def delete_destination(request,pk):
-    pass
+    api_url = f'http://127.0.0.1:8000/details/{pk}'
+    try:
+        response = requests.delete(api_url)
+        if response.status_code == 200 or response.status_code == 204:
+            messages.success(request,'Item Successfully Deleted')
+            return redirect('/list_destination')
+        else:
+            messages.error(request, f'Something went wrong: {response.status_code}')
+    except requests.RequestException as e:
+        messages.error(request, f'Something went wrong: Error: {e}')
+    return redirect(f'/details_destination/{pk}')
+
